93c8b303e9d80e38b99b3767016d7f2ebe6743de
23BigMike <miguemontu23@gmail.com>
2025-12-03 20:26:05 -0500

    Primer commit del proyecto Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Contact } from './pages/contact/contact';
+import { Home } from './pages/home/home';
+import{Tests} from './pages/tests/tests'
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'Contacto', component: Contact },
+  { path: 'Simulacros', component: Tests },
+  { path: '**', redirectTo: '' }
+];
